198809289772e2dd936ccc3297a3c3635ee238d4
Bashblubb <reimar.langhammer@gmx.de>
2015-12-03 18:24:18 +0100

    Mastermind hinzugefügt

---
+++ Mastermind/src/Mastermind.java
@@ -0,0 +1,12 @@
+
+//Mastermind wir um die Klasse ui erweitert
+public class Mastermind extends Ui {
+
+	public static void main(String[] args) {
+		Mastermind spiel = new Mastermind();
+		//spiel.ui();
+		spiel.menue();
+			}
+	
+	
+}

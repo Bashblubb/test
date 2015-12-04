bd1115153c5d7f8fd33292aab85309f6eb2c7bb1
Bashblubb <reimar.langhammer@gmx.de>
2015-12-04 16:57:55 +0100

    HackerRank added

--- CodeEval/src/Programme/ArmstrongNumber.java
+++ CodeEval/src/Programme/ArmstrongNumber.java
@@ -3,14 +3,40 @@ package Programme;
 public class ArmstrongNumber {
 
 	public static void main(String[] args) {
-		String zahl = "12345";
-	
-		for (int i = 0;i<zahl.length();i++){
-			System.out.println(zahl.toCharArray());
-		}
+	int zahl = 153;
 
+		boolean isNumber = validateNumbers(zahl);
+		
+		if (isNumber == true){
+			System.out.println(zahl + " ist eine Armstrongzahl!");
+		}else{
+			System.out.println(zahl +" ist keine Armstrongzahl!");
+		}
 	}
 	
-
+	
+	
+	static boolean validateNumbers(int zahl){
+		int ergebnis = 0;
+		String s = String.valueOf(zahl);
+		int[] zahlen = new int[s.length()];
+		for (int i = 0; i< zahlen.length;i++){
+			zahlen[i] = Integer.parseInt(String.valueOf(s.charAt(i)));
+		}
+		
+		for(int j = 0; j<zahlen.length;j++){
+		ergebnis += Math.pow(zahlen[j], zahlen.length);
+		}
+		
+		if(ergebnis == zahl){
+			return true;
+		}else{
+			return false;
+		}
+		
+	
+		
+		
+	}
 
 }

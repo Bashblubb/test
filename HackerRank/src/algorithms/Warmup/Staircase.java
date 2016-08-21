b187e2b24d04bc6b87e0733c4c2f10cc1ca8291c
Bashblubb <reimar.langhammer@gmx.de>
2016-08-21 17:37:18 +0200

    Algorithms Warmup Staircase created

---
+++ HackerRank/src/algorithms/Warmup/Staircase.java
@@ -0,0 +1,17 @@
+package algorithms.Warmup;
+
+import java.util.Scanner;
+
+public class Staircase {
+
+	private static int anzahl;
+	public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	anzahl = in.nextInt();
+	for(int i = 0; i< anzahl; i++){
+		
+	}
+
+	}
+
+}
